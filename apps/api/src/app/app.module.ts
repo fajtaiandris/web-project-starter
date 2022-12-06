cd5f343fa5bcec3ef427938271f487366880d512
Fajtai Andris <afajtai@precognoxcom>
2022-12-06 22:00:38 +0100

    added jwt authentication for api

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { ProductsModule } from '@web-project-starter/products';
+import { AuthModule } from '../auth/auth.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
